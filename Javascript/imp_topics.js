130b6089e3ed1f5390d96aeb8f39e4a7eb005ae0
Ram Kumar Mahto <ramkumar@Rams-MacBook-Air.local>
2023-09-24 12:19:07 +0530

    event bubbling and capturing

--- Javascript/imp_topics.js
+++ Javascript/imp_topics.js
@@ -5,6 +5,31 @@
 
 // event bubbling, capturing, event propagation, stop propagation
 
+// event propagated by default in first from parent to child and when event is triggered then from child to parent
+// what is event.preventdefault()
+// using this we can stop default behaviour of event or you could say of element
+
+const divEle = document.querySelector('div')
+const btn = document.querySelector('#btn');
+const body = document.querySelector('body')
+
+divEle.style.border = "2px solid green";
+divEle.addEventListener("click", (event)=>{
+    console.log("div clicked!");
+    divEle.style.backgroundColor = "tomato";
+   
+})
+
+btn.addEventListener('click', (event)=>{
+// event.stopPropagation()
+    console.log('btn clicked!')
+})
+
+body.addEventListener('click', ()=>{
+    console.log("body triggerd!")
+}) 
+
+
 // debouncing, throttling
 
 
